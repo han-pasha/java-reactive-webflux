2456baf063ec4a9ca312c2d110f272a59eeea3a8
Han Pasha <affanda.pasha@xtremax.com>
2022-01-26 09:53:26 +0700

    Stable, After Adding Guava

--- src/main/java/com/reactiveweb/reactiveprogramming/service/impl/TestingServiceImpl.java
+++ src/main/java/com/reactiveweb/reactiveprogramming/service/impl/TestingServiceImpl.java
@@ -1,12 +1,16 @@
 package com.reactiveweb.reactiveprogramming.service.impl;
 
+import com.google.common.base.Preconditions;
 import com.reactiveweb.reactiveprogramming.model.TestingModel;
 import com.reactiveweb.reactiveprogramming.repository.TestingRepo;
 import com.reactiveweb.reactiveprogramming.service.TestingService;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -17,10 +21,17 @@ public class TestingServiceImpl implements TestingService {
      * THIS REPO COULDN'T BE AUTO WIRED BECAUSE
      * R2DBC DOESN'T ALLOW BEAN ON ITS REPO
      */
+    @Autowired
     private TestingRepo testingRepo;
 
     @Override
     public Mono<TestingModel> createTestingModel(TestingModel model) {
-        return testingRepo.save(model);
+        Optional<Mono<TestingModel>>  test = Optional.of(Mono.just(model));
+//        Preconditions.checkArgument()
+        Preconditions.checkNotNull(test);
+        return Mono.just(model)
+                .flatMap(x -> testingRepo.save(x))
+                .doOnSuccess(x -> System.out.println("Welp"))
+                .switchIfEmpty(Mono.empty());
     }
 }
--- src/main/java/com/reactiveweb/reactiveprogramming/web/rest/TestingController.java
+++ src/main/java/com/reactiveweb/reactiveprogramming/web/rest/TestingController.java
@@ -4,13 +4,9 @@ import com.reactiveweb.reactiveprogramming.model.TestingModel;
 import com.reactiveweb.reactiveprogramming.service.TestingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-@Component
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -18,9 +14,6 @@ public class TestingController {
 
     @Autowired
     private final TestingService testingService;
-    @Qualifier("getWebClientBuilder")
-    @Autowired
-    private WebClient.Builder client;
 
     @GetMapping("/welcome")
     Mono<String> welcome() {
@@ -29,6 +22,6 @@ public class TestingController {
 
     @PostMapping("/testing/create")
     Mono<TestingModel> create(@RequestBody TestingModel model) {
-        return testingService.createTestingModel(model);
+       return testingService.createTestingModel(model);
     }
 }
